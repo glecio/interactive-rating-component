6beeee3390be44f7f73ddb49e66a5812d8b6b5c4
Glecio Santos <glecio.santos@gmail.com>
2022-07-10 18:31:49 -0300

    Thankyou page getting parameter

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
+
 
 function Thankyou() {
+    let params = useParams();
     return (
-        <h1>this is the thankyou</h1>
+        <>
+            <h1>You choose : {params.rating} </h1>
+        </>
+
     );
 }
 
