c77f778b0fd427a296a4f98efd1b4f4e31ed232d
Glecio Santos <glecio.santos@gmail.com>
2022-07-10 18:32:33 -0300

    Getting values from components with hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Numbers from '../components/number/Numbers';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const [rating, setRating] = useState('');
+    const [parentData, setParentData] = useState('');
+    
+    const sendChildToParent = (rating) => {
+        setRating(rating);
+        setParentData(rating);
+    };
+
+
     return (
+
         <>
             <div className="container">
                 <div className="card">
                     <h1>How did we do?</h1>
                     <p>Please let us know how we did with your support request. All feedback is appreciated 
                     to help us improve our offering!</p>
-                    <Numbers /> 
+                    <Numbers sendChildToParent={sendChildToParent}  /> 
+                    <br/>
+                    <Link onClick={() => setParentData(rating)} to={`/thankyou/${rating}`}>
+                        Enviar
+                    </Link>
                 </div>
             </div>
         </>
