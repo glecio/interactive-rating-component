8595ce798a2bebdca8db873ffdf26637917419ab
Glecio Santos <glecio.santos@gmail.com>
2022-07-10 18:31:26 -0300

    Added li tags instead span to render links

--- src/components/number/Numbers.jsx
+++ src/components/number/Numbers.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import './numbers.css'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
+const Number = ({sendChildToParent} ) => {
+  const [isActive, setIsActive] = useState('5');
+ 
 
-const Number = () => {
-  const [activeNav, setActiveNav ] = useState('#')
+  useEffect(() => {
+    console.log('use effects')
+    sendChildToParent(isActive)
+    
+  }, []);
+  const handleClick = (event) => {
+    sendChildToParent(event.target.getAttribute('value'))
+    const rating = event.target.getAttribute('value')
+    setIsActive(rating);
+  }
+
   return (
-      <div className="numbers">
-        <span onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} > 1</span>
-        <span onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} > 2</span>
-        <span onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} > 3</span>
-        <span onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>4</span>
-        <span onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> 5</span>
-      </div>
+    <>
+      <ul className="numbers">
+        <li onClick={handleClick} value='1' className={isActive === '1' ? 'active' : ''}>1</li>
+        <li onClick={handleClick} value='2' className={isActive === '2' ? 'active' : ''}>2</li>
+        <li onClick={handleClick} value='3' className={isActive === '3' ? 'active' : ''}>3</li>
+        <li onClick={handleClick} value='4' className={isActive === '4' ? 'active' : ''}>4</li>
+        <li onClick={handleClick} value='5' className={isActive === '5' ? 'active' : ''}>5</li>
+      </ul>
+    </>
   )
 }
 
