89e123d097fe625a92801d8fd6704f89c8d1d7a7
Glecio Santos <glecio.santos@gmail.com>
2022-07-11 16:57:52 -0300

    console.log removal

--- src/components/number/Numbers.jsx
+++ src/components/number/Numbers.jsx
@@ -8,10 +8,9 @@ const Number = ({sendChildToParent} ) => {
  
 
   useEffect(() => {
-    console.log('use effects')
     sendChildToParent(isActive)
-    
   }, []);
+
   const handleClick = (event) => {
     sendChildToParent(event.target.getAttribute('value'))
     const rating = event.target.getAttribute('value')
@@ -19,7 +18,7 @@ const Number = ({sendChildToParent} ) => {
   }
 
   return (
-    <>
+    <div>
       <ul className="numbers">
         <li onClick={handleClick} value='1' className={isActive === '1' ? 'active' : ''}>1</li>
         <li onClick={handleClick} value='2' className={isActive === '2' ? 'active' : ''}>2</li>
@@ -27,7 +26,7 @@ const Number = ({sendChildToParent} ) => {
         <li onClick={handleClick} value='4' className={isActive === '4' ? 'active' : ''}>4</li>
         <li onClick={handleClick} value='5' className={isActive === '5' ? 'active' : ''}>5</li>
       </ul>
-    </>
+    </div>
   )
 }
 
