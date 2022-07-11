c8f5c1b79800ee3a3933a1a30964e54bb004e7b9
Glecio Santos <glecio.santos@gmail.com>
2022-07-11 16:57:28 -0300

    Star image and Button to submit rating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
+import { ReactComponent as Star } from '../assets/icon-star.svg';
+
 import Numbers from '../components/number/Numbers';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
     const [rating, setRating] = useState('');
-    const [parentData, setParentData] = useState('');
     
     const sendChildToParent = (rating) => {
         setRating(rating);
-        setParentData(rating);
     };
 
-
     return (
-
         <>
             <div className="container">
                 <div className="card">
+                    <Star className='star-img' />
                     <h1>How did we do?</h1>
                     <p>Please let us know how we did with your support request. All feedback is appreciated 
                     to help us improve our offering!</p>
                     <Numbers sendChildToParent={sendChildToParent}  /> 
                     <br/>
-                    <Link onClick={() => setParentData(rating)} to={`/thankyou/${rating}`}>
+                    <Link to={`/thankyou/${rating}`} className='btn-send'>
                         Enviar
                     </Link>
                 </div>
