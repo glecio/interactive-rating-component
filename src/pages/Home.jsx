48064708e79b0b5dddc1e3137429ea632689cba9
Glecio Santos <glecio.santos@gmail.com>
2022-07-13 18:03:02 -0300

    Star image container markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,14 +16,17 @@ function Home() {
         <>
             <div className="container">
                 <div className="card">
-                    <Star className='star-img' />
+                    <div className="card-img">
+                        <Star className='star-img' />
+                    </div>
+                    
                     <h1>How did we do?</h1>
                     <p>Please let us know how we did with your support request. All feedback is appreciated 
                     to help us improve our offering!</p>
                     <Numbers sendChildToParent={sendChildToParent}  /> 
                     <br/>
                     <Link to={`/thankyou/${rating}`} className='btn-send'>
-                        Enviar
+                        SUBMIT
                     </Link>
                 </div>
             </div>
