61b81644f7d6a27ca0ffac3a107d9521770a5ea8
Glecio Santos <glecio.santos@gmail.com>
2022-07-13 18:04:54 -0300

    Initial markup for thankyou page

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -6,7 +6,16 @@ function Thankyou() {
     let params = useParams();
     return (
         <>
-            <h1>You choose : {params.rating} </h1>
+            <div className="container">
+                <div className="card">
+                    <h1>How did we do?</h1>
+                    <p>Please let us know how we did with your support request. All feedback is appreciated 
+                    to help us improve our offering!</p>
+                    <br/>
+                    <h1>You choose : {params.rating} </h1>
+                </div>
+            </div>
+            
         </>
 
     );
